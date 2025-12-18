61b700f435be2e62ed38fa0a9a78588d8d821bae
Vsahu <vsahu53891@gmail.com>
2025-12-18 10:56:11 +0530

    git commit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactRoute from "./Routes/ReactRoute.jsx";
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import MovieDetail from './components/MovieDetail';
+import MoveSlider from './components/MoveSlider';
+import ContactUs from './components/ContactUs';
+import './App.css';
+import './CSS/Home.css';
+import './CSS/Login.css';
+import './CSS/MoveSlider.css';
+import './CSS/MovieDetail.css';
+import './CSS/ContactUs.css';
+
+
 
 function App() {
   return (
