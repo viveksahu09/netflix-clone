c255d53882a83a4ffb0ee01fb0ec3e1b30d3d99e
Vsahu <vsahu53891@gmail.com>
2025-12-18 11:10:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactRoute from "./Routes/ReactRoute.jsx";
-import HomePage from './components/HomePage';
-import Login from './components/Login';
-import MovieDetail from './components/MovieDetail';
-import MoveSlider from './components/MoveSlider';
-import ContactUs from './components/ContactUs';
-import FeedbackList from './components/FeedbackList';
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import MovieDetail from "./components/MovieDetail";
+import MoveSlider from "./components/MoveSlider";
+import ContactUs from "./components/ContactUs";
+import FeedbackList from "./components/FeedbackList";
 
-import './App.css';
-import './CSS/Home.css';
-import './CSS/Login.css';
-import './CSS/MoveSlider.css';
-import './CSS/MovieDetail.css';
-import './CSS/ContactUs.css';
+import "./App.css";
+import "./CSS/Home.css";
+import "./CSS/Login.css";
+import "./CSS/MoveSlider.css";
+import "./CSS/MovieDetail.css";
+import "./CSS/ContactUs.css";
 
-import backImg from '../assets/back-img.jpg';
-<img src={backImg} />
-
-
+import backImg from "../assets/back-img.jpg";
+<img src={backImg} />;
 
 function App() {
   return (
